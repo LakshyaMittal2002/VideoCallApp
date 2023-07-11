e1d520b290ea51fe11318d3bedee596dc3236e09
LakshyaMittal2002 <83869165+LakshyaMittal2002@users.noreply.github.com>
2023-07-11 13:57:52 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+// import { APP_ID, SERVER_SECRET } from './constant';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
 }
 
-export default App;
+export function getUrlParams(
+  url = window.location.href
+) {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function App() {
+  const roomID = getUrlParams().get('roomID') 
+  // const roomID = "lakshya";
+  let myMeeting = async (element) => {
+ // generate Kit Token
+  const appID = 1203201337;
+  const serverSecret = "53ffd2aa667594b0392476e312c41a71";
+  const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+  // const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  Date.now().toString(),  "Asha");
+
+
+ // Create instance object from Kit Token.
+  const zp = ZegoUIKitPrebuilt.create(kitToken);
+  // start the call
+  zp.joinRoom({
+    container: element,
+    sharedLinks: [
+      {
+        name: 'Personal link',
+        url:
+         window.location.protocol + '//' + 
+         window.location.host + window.location.pathname +
+          '?roomID=' +
+          roomID,
+      },
+    ],
+    scenario: {
+      mode: ZegoUIKitPrebuilt.GroupCall,
+     // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      //  mode: ZegoUIKitPrebuilt.OneONoneCall,
+    
+    },
+    showRoomTimer:true
+  });
+
+
+};
+
+return (
+<div
+  className="myCallContainer"
+  ref={myMeeting}
+  style={{ width: '100vw', height: '100vh' }}
+></div>
+);
+}
